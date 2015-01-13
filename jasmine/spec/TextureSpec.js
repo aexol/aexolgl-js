896d6192a45fc7278ee2a765a79ebbae1e8cbe79
Dawid Lenkiewicz <dawid.lenkiewicz86@gmail.com>
2015-01-13 16:46:50 +0100

    new specs

--- jasmine/spec/TextureSpec.js
+++ jasmine/spec/TextureSpec.js
@@ -16,4 +16,28 @@ describe("Texture", function() {
     expect(texture.type).toEqual(555);
   });
 
+  describe('#handle function', function() {
+
+    it('should call handleAtlas if isAtlas', function() {
+      texture = new Texture();
+      texture.isAtlas = true;
+
+      spyOn(texture, 'handleAtlas');
+
+      texture.handle();
+      expect(texture.handleAtlas).toHaveBeenCalled();
+    });
+
+    it('should call handle2DTexture unless isAtlas', function() {
+      texture = new Texture();
+      texture.isAtlas = false;
+
+      spyOn(texture, 'handle2DTexture');
+
+      texture.handle();
+      expect(texture.handle2DTexture).toHaveBeenCalled();
+    });
+
+  });
+
 });

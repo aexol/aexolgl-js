9ca7bba28a5bf1cf84e0c3d31dd3763bf44e0da0
aexol <aexol@aexol.com>
2015-07-13 14:40:31 +0200

    c

---
+++ docs/api.js
@@ -0,0 +1,123 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Aex",
+        "Animation",
+        "Buffer",
+        "Camera",
+        "Fog",
+        "Frustum",
+        "GameObject",
+        "Indexer",
+        "Label",
+        "Light",
+        "MObject",
+        "Material",
+        "Matrix",
+        "Matrix3",
+        "Mesh",
+        "Pivot",
+        "Quaternion",
+        "Resource",
+        "Scene",
+        "Shader",
+        "Shadow",
+        "TextNode",
+        "Texture",
+        "Vector",
+        "Vector4",
+        "ZBuffer",
+        "aexolGL"
+    ],
+    "modules": [
+        "Animation",
+        "Buffer",
+        "Fog",
+        "Font",
+        "Game",
+        "Light",
+        "Math",
+        "Mesh",
+        "Physics",
+        "Resource",
+        "ShaderLibrary",
+        "Shadow",
+        "aexolGL",
+        "camera",
+        "materialLib",
+        "start",
+        "utils"
+    ],
+    "allModules": [
+        {
+            "displayName": "aexolGL",
+            "name": "aexolGL"
+        },
+        {
+            "displayName": "Animation",
+            "name": "Animation",
+            "description": "All animations are executed during window.logic loop"
+        },
+        {
+            "displayName": "Buffer",
+            "name": "Buffer"
+        },
+        {
+            "displayName": "camera",
+            "name": "camera"
+        },
+        {
+            "displayName": "Fog",
+            "name": "Fog"
+        },
+        {
+            "displayName": "Font",
+            "name": "Font"
+        },
+        {
+            "displayName": "Game",
+            "name": "Game"
+        },
+        {
+            "displayName": "Light",
+            "name": "Light"
+        },
+        {
+            "displayName": "materialLib",
+            "name": "materialLib"
+        },
+        {
+            "displayName": "Math",
+            "name": "Math"
+        },
+        {
+            "displayName": "Mesh",
+            "name": "Mesh"
+        },
+        {
+            "displayName": "Physics",
+            "name": "Physics"
+        },
+        {
+            "displayName": "Resource",
+            "name": "Resource"
+        },
+        {
+            "displayName": "ShaderLibrary",
+            "name": "ShaderLibrary"
+        },
+        {
+            "displayName": "Shadow",
+            "name": "Shadow"
+        },
+        {
+            "displayName": "start",
+            "name": "start"
+        },
+        {
+            "displayName": "utils",
+            "name": "utils"
+        }
+    ]
+} };
+});
